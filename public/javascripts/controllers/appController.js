3f899b29e6894bd45a516556f31b7c0719c8afa2
judas1017 <judaszhangly@gmail.com>
2014-09-03 11:56:13 +0800

    create a notification trigger

--- public/javascripts/controllers/appController.js
+++ public/javascripts/controllers/appController.js
@@ -79,6 +79,8 @@ if (app) {
     // Listen to node.creating event from NodeController
     // for requesting to create a new node in the tree
     $scope.$on('node.creating', function (event, scope) {
+      $scope.closePopup();
+      showNotification({type: 'success', content: 'Test'});
 
       // todo: send the create request to server side and send back the result
       //$scope.$broadcast('node.created', {});
@@ -87,6 +89,8 @@ if (app) {
     // Listen to node.deleting event from NodeController
     // for requesting to delete specific node in the tree
     $scope.$on('node.removing', function (event, scope) {
+      $scope.closePopup();
+      showNotification({type: 'failure', content: 'Test'});
 
       // todo: send the delete request to server side and send back the result
       //$scope.$broadcast('node.removed', {});
@@ -95,6 +99,17 @@ if (app) {
     $scope.closePopup = function () {
       $scope.animations.popup = false;
       $scope.popup = {};
+    };
+
+    var showNotification = function (notification) {
+      $scope.animations.notification = true;
+
+      $scope.notification.type = notification.type;
+      $scope.notification.content = notification.content;
+
+      $timeout(function () {
+        $scope.animations.notification = false;
+      }, $rootScope.notifcationInterval);
     };
   }]);
 }
