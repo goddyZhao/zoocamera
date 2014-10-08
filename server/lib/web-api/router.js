87954625c9ba6c3778cb4469d9492d9fc2f41756
wkliu <wkliu@successfactors.com>
2014-10-09 00:18:08 +0800

    Auto connect to zookeeper for test api page

--- server/lib/web-api/router.js
+++ server/lib/web-api/router.js
@@ -2,9 +2,14 @@
 
 var express = require('express');
 var router = express.Router();
+var logger = require('../../logger').create('web-api');
 
 router.get('/test-api', function (req, res) {
-  res.render('test-api');
+  var data = new Object();
+  if(req.query.host && req.query.port) {
+  	data = {zkHost: req.query.host, zkPort: req.query.port};
+  }
+  res.render('test-api', data);
 });
 
 module.exports = router;
